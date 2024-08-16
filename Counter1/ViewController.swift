6257fcdc7614c6285697fa7aa32ef927c665777e
Mike <misha995@yandex.ru>
2024-08-16 22:44:38 +0300

    first commit

---
+++ Counter1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Counter1
+//
+//  Created by Михаил on 16.08.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
